cf9d7341a389587f644040409f5ee0bb72b6b90b
divquan <97671400+divquan@users.noreply.github.com>
2023-03-18 09:43:01 +0000

    fixes to database connection

--- db.js
+++ db.js
@@ -4,18 +4,26 @@ dotenv.config();
 
 const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
 
-export const db = mysql.createConnection({
+export const db = mysql.createPool({
   host: DB_HOST,
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_DATABASE,
+  connectionLimit: 10,
 });
+var now = new Date();
+console.log(`Connected to database:${now.toUTCString()}`);
+// db.connect(function (err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
 
-db.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+// });
 
-  console.log("connected as id " + db.threadId);
-});
+// export const db = mysql.createConnection({
+//   host: DB_HOST,
+//   user: DB_USER,
+//   password: DB_PASSWORD,
+//   database: DB_DATABASE,
+// });
