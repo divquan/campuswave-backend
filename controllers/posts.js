876c0aa78d75ab33a2b6775a37b27d06ade5ad68
divquan <97671400+divquan@users.noreply.github.com>
2023-03-21 21:51:50 +0000

    removing multer functions

--- controllers/posts.js
+++ controllers/posts.js
@@ -22,29 +22,30 @@ export const getPost = (req, res) => {
   });
 };
 export const addPost = (req, res) => {
-  const token = req.cookies.access_token;
-  if (!token) return res.status(401).json("Not authenticated!");
+  // const token = req.cookies.access_token;
+  // if (!token) return res.status(401).json("Not authenticated!");
+  console.log(req.body);
 
-  jwt.verify(token, "jwtkey", (err, userInfo) => {
-    if (err) return res.status(403).json("Token is not valid!");
+  // jwt.verify(token, "jwtkey", (err, userInfo) => {
+  //   if (err) return res.status(403).json("Token is not valid!");
 
-    const q =
-      "INSERT INTO posts(`title`, `desc`, `img`, `cat`, `date`,`uid`) VALUES (?)";
+  //   const q =
+  //     "INSERT INTO posts(`title`, `desc`, `img`, `cat`, `date`,`uid`) VALUES (?)";
 
-    const values = [
-      req.body.title,
-      req.body.desc,
-      req.body.img,
-      req.body.cat,
-      req.body.date,
-      userInfo.id,
-    ];
+  //   const values = [
+  //     req.body.title,
+  //     req.body.desc,
+  //     req.body.img,
+  //     req.body.cat,
+  //     req.body.date,
+  //     userInfo.id,
+  //   ];
 
-    db.query(q, [values], (err, data) => {
-      if (err) return res.status(500).json(err);
-      return res.json("Post has been created.");
-    });
-  });
+  //   // db.query(q, [values], (err, data) => {
+  //   //   if (err) return res.status(500).json(err);
+  //   //   return res.json("Post has been created.");
+  //   // });
+  // });
 };
 export const editPost = (req, res) => {};
 export const deletePost = (req, res) => {};
--- index.js
+++ index.js
@@ -10,21 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-app.post("/api/upload", upload.single("file"), function (req, res) {
-  const file = req.file;
-  res.status(200).json(file.filename);
-});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
