faae8c8c75df19584cbaabe19b76f6beaff37e4c
divquan <97671400+divquan@users.noreply.github.com>
2023-03-22 01:44:55 +0000

    fixes to addposts

--- controllers/posts.js
+++ controllers/posts.js
@@ -25,7 +25,7 @@ export const addPost = (req, res) => {
   // const token = req.cookies.access_token;
   // if (!token) return res.status(401).json("Not authenticated!");
   console.log(req.body);
-  res.json(req.body, "Done").status(200);
+  res.json({ message: "Done" }).status();
 
   // jwt.verify(token, "jwtkey", (err, userInfo) => {
   //   if (err) return res.status(403).json("Token is not valid!");
