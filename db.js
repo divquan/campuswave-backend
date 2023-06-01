a29580486719472b6b6acd91fa86d916a13dd52f
Divine Quansah <divquan@gmail.com>
2023-06-01 14:06:51 +0000

    modified: database config

--- db.js
+++ db.js
@@ -1,29 +1,8 @@
-import mysql from "mysql";
+import mysql from "mysql2";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
-
-export const db = mysql.createPool({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
-  connectionLimit: 10,
-});
-var now = new Date();
-console.log(`Connected to database:${now.toUTCString()}`);
-// db.connect(function (err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-
-// });
-
-// export const db = mysql.createConnection({
-//   host: DB_HOST,
-//   user: DB_USER,
-//   password: DB_PASSWORD,
-//   database: DB_DATABASE,
-// });
+const { DATABASE_URL } = process.env;
+export const db = mysql.createConnection(process.env.DATABASE_URL);
+console.log("Connected to database!");
+db.end();
